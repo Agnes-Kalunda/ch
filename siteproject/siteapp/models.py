20d911f7e1ee3504f0be513c2d6f84e2faf8bd5a
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-06 22:19:52 +0300

    load the CSV file with the stock data

--- siteproject/siteapp/models.py
+++ siteproject/siteapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import plotly
+import json
+import os
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+import pickle
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pandas as pd
+
 
 # Create your models here.
+
+
+#loading the dataset
+dataset_train = pd.read_csv('./CSV.csv')
+dataset_train.head()
+
+#using stock price column to train the model
+
+training_set = dataset_train.iloc[:,1:2].values
+
+print(training_set)
+print(training_set.shape)
--- siteproject/siteproject/settings.py
+++ siteproject/siteproject/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'siteproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER':'',
-        'PASSWORD':''
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'sitedatabase',
+        'USER':'agnes',
+        'PASSWORD':'Kalundakaluu',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
 
