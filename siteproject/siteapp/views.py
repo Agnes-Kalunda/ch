1442c7cf5b77fdd40efc0b67be83e9be20736e76
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-06 13:43:10 +0300

    created template views

--- siteproject/siteapp/views.py
+++ siteproject/siteapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render (request, 'home.html')
+
+def nav(request):
+    return render (request, 'navbar.html' )
+
+def base(request):
+    return render (request, 'base.html')
