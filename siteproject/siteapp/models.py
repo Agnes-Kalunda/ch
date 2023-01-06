fe200e6233b89af460463ea6d2265a928c5e05eb
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-06 23:19:37 +0300

    reshaped the data

--- siteproject/siteapp/models.py
+++ siteproject/siteapp/models.py
@@ -8,6 +8,10 @@ import pickle
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense 
+from keras.layers import Dropout
 
 
 # Create your models here.
@@ -48,3 +52,19 @@ print(y_train)
 #reshaping the data
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 x_train.shape
+
+regressor = Sequential()
+
+regressor.add(LSTM(units = 50, return_sequences= True, input_shape = (x_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units =50, return_sequences =True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units =50, return_sequences= True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units= 50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units=1))
